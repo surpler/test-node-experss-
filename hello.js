07e48bcb01d987825a6930fe48f110634a461856
surpler <1471197967@qq.com>
2023-04-01 14:03:29 +0800

    提交文件

--- hello.js
+++ hello.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(80, () => {
-    console.log('示例应用正在监听 3000 端口 !');
+app.listen(8080, () => {
+    console.log('示例应用正在监听 8080 端口 !');
 });
